60ebff123289a2d2f351e16501dc70118669a8d9
davidfullerton <david.w.fullerton@blueyonder.co.uk>
2017-02-02 17:10:22 +0000

    Added scan to basket model

--- app/models/basket.rb
+++ app/models/basket.rb
@@ -1,3 +1,15 @@
 class Basket < ApplicationRecord
   has_many :items
+
+  def scan(product_code)
+    catalog_entries = CatalogEntry.where(product_code: product_code)
+    if catalog_entries.count > 1
+      Rails.logger.error('Multiple catalog entries with same product code found')
+    elsif catalog_entries.count < 1
+      Rails.logger.error('Invalid code given, item not added to basket')
+    else
+      catalog_entry = catalog_entries.first
+      self.items.create!(product_code: product_code, name: catalog_entry.name, price: catalog_entry.price, vat: catalog_entry.price * catalog_entry.vat_rate)
+    end
+  end
 end
