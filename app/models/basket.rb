c9175e70b95adb24e490a0f78d3c28d988a42b0e
davidfullerton <david.w.fullerton@blueyonder.co.uk>
2017-02-02 17:52:32 +0000

    Added methods subtotal, total_vat, flag_discount, total_discount and total to basket

--- app/models/basket.rb
+++ app/models/basket.rb
@@ -1,6 +1,13 @@
 class Basket < ApplicationRecord
   has_many :items
 
+=begin
+scanning the code of a product creates an item by finding the equivilant
+row in the catalog entry table. It creates this new item and calculates the
+VAT that will be applied, storing this in the item entry. The item is
+given the id of the current basket.
+=end
+
   def scan(product_code)
     catalog_entries = CatalogEntry.where(product_code: product_code)
     if catalog_entries.count > 1
@@ -12,4 +19,36 @@ class Basket < ApplicationRecord
       self.items.create!(product_code: product_code, name: catalog_entry.name, price: catalog_entry.price, vat: catalog_entry.price * catalog_entry.vat_rate)
     end
   end
+
+  #calculates total price of items before any discounts or VAT.
+  def subtotal
+    items.pluck(:price).reduce(:+)
+  end
+
+  #sums the total vat of all items in the basket
+  def total_vat
+    items.pluck(:vat).reduce(:+)
+  end
+
+  #discount of £5 given for every second flag
+  def flag_discount
+    flag_count = items.where(product_code: 1).count
+    (flag_count/2) * 5
+  end
+
+  #if subtotal + VAT - flagdiscount is over £70, %12 discount is applied
+  def total_discount
+    total = subtotal + total_vat - flag_discount
+    if total > 70
+      return (total) * 0.12
+    else
+      return 0
+    end
+  end
+
+  #final total, including VAT and discounts. Rounded to 2 decimal places
+  def total
+    (subtotal + total_vat - flag_discount - total_discount).round(2)
+  end
+
 end
